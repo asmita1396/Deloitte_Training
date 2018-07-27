718e83dad8d7fcf906f389df1fcc23e60ed29f61
asmita <asmita1396@users.noreply.github.com>
2018-07-27 18:46:05 +0530

    Core Java Assignment 1

---
+++ CoreJavaAssignment1/src/com/deloitte/main/Arithmetic.java
@@ -0,0 +1,27 @@
+package com.deloitte.main;
+
+public abstract class Arithmetic {
+	protected int num1;
+	protected int num2;
+	public int getNum1() {
+		return num1;
+	}
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	Arithmetic(){
+		
+	}
+	Arithmetic(int n1, int n2){
+		this.num1=n1;
+		this.num2=n2;
+		
+	}
+	public abstract int calculate();
+}
